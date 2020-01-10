827dd0abac493613556da89bb3235fcc818032df
Nassoko <sira.nassoko20@uscs1-8.trinity.nyc.ny.us>
2020-01-10 14:43:37 -0500

    fixed port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(favicon(__dirname + '/public/images/logo.png'));
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Server started at ' + new Date() + ', on port ' + port + '!');
 });
